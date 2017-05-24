d20517c0b72171138106b96f231c49e9fb594787
Garik Kalashyan <garik.kalash@gmail.com>
2017-05-24 11:35:55 +0400

    sdasd

--- server.js
+++ server.js
@@ -7,3 +7,10 @@ app.use(express.static(__dirname + '/dist'));
 // Heroku port
 app.listen(process.env.PORT || 8080);
 
+const path = require('path');
+// ...
+// For all GET requests, send back index.html
+// so that PathLocationStrategy can be used
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
