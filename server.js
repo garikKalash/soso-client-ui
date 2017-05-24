cf09c874f9fe2f9e8ed1f00d67076341633f9d6b
Garik Kalashyan <garik.kalash@gmail.com>
2017-05-24 11:42:42 +0400

    asdasd

--- server.js
+++ server.js
@@ -8,6 +8,22 @@ app.use(express.static(__dirname + '/dist'));
 app.listen(process.env.PORT || 8080);
 
 const path = require('path');
+
+const forceSSL = function() {
+  return function (req, res, next) {
+    if (req.headers['x-forwarded-proto'] !== 'https') {
+      return res.redirect(
+        ['https://', req.get('Host'), req.url].join('')
+      );
+    }
+    next();
+  }
+}
+// Instruct the app
+// to use the forceSSL
+// middleware
+app.use(forceSSL());
+
 // ...
 // For all GET requests, send back index.html
 // so that PathLocationStrategy can be used
